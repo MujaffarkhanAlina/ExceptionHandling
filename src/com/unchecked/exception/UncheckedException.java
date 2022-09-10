6e1ec69b82e68d3998ffaa100b400a6dde0088f3
MujaffarkhanAlina <mujaffar.khan08@gmail.com>
2022-09-10 18:30:19 -0400

    Exception Handling

---
+++ src/com/unchecked/exception/UncheckedException.java
@@ -0,0 +1,10 @@
+package com.unchecked.exception;
+
+public class UncheckedException extends RuntimeException
+{
+	   public UncheckedException(String message) 
+	   {
+	      // Call the Constructor of RuntimeException Class
+	      super(message);
+	   }
+}
